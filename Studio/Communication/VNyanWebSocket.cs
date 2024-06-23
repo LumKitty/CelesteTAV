545384cc8afe9f97d30f72430f83d92fb8c3b626
lumkitty <github@lum.uk>
2024-06-23 22:38:08 +0100

    RC3 Update wind data to work with new wind node in VNyan 1.4.2 Update how hair colour is sent to work with the Text to Colour node Add a working settings file for configuration
    Baring any last minute bugs this is basically a 1.0 final release

--- Studio/Communication/VNyanWebSocket.cs
+++ Studio/Communication/VNyanWebSocket.cs
@@ -7,38 +7,77 @@ CameraPos: {Level.Camera.Position}
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CelesteStudio.RichText;
 using Microsoft.VisualBasic;
 using StudioCommunication;
 using WatsonWebsocket;
 
 namespace CelesteStudio.Communication;
+
 public static class VNyanWebSocket {
     
+    private static string GetSetting(string param) {
+        string result = "";
+        string line;
+        string setting;
+        int pos;
+        bool done = false;
+        param = param.ToLower();
+        //Pass the file path and file name to the StreamReader constructor
+        StreamReader sr = new StreamReader(".\\CelesteTAV.config");
+        //Read the first line of text
+        line = sr.ReadLine();
+        //Continue to read until you reach end of file
+        while ((result == "") && (line != null)) {
+            pos = line.IndexOf('=');
+            setting = line.Substring(0, pos).Trim().ToLower();
+
+            if (setting.Trim().ToLower() == param) {
+                result = line.Substring(pos + 1);
+            }
+            line = sr.ReadLine();
+        }
+        //close the file
+        sr.Close();
+        if (result=="") {
+            Console.WriteLine("Could not find setting: " + param);
+        }
+        Console.WriteLine("Read setting: "+param+" Result: " + result);
+        return result;
+    }
+
+    private static int GetSettingInt(string param) {
+        return Convert.ToInt32(GetSetting(param));
+    }
+
     private static WatsonWsClient wsClient;
     private static bool wsConnected = false;
     private static System.Threading.CancellationToken CT = new System.Threading.CancellationToken();
-    private static string VNyanURL= Properties.Settings.Default.VNyanURL;
-    private static int DangerZoneX1 = Properties.Settings.Default.VTuberDangerZoneX1;
-    private static int DangerZoneX2 = Properties.Settings.Default.VTuberDangerZoneX2;
-    private static int DangerZoneY1 = Properties.Settings.Default.VTuberDangerZoneY1;
-    private static int DangerZoneY2 = Properties.Settings.Default.VTuberDangerZoneY2;
-    private static int SafeZoneX1 = Properties.Settings.Default.VTuberSafeZoneX1;
-    private static int SafeZoneX2 = Properties.Settings.Default.VTuberSafeZoneX2;
-    private static int SafeZoneY1 = Properties.Settings.Default.VTuberSafeZoneY1;
-    private static int SafeZoneY2 = Properties.Settings.Default.VTuberSafeZoneY2;
+    //private static string VNyanURL = Properties.Settings.Default.VNyanURL;
+    private static string VNyanURL = GetSetting("VNyanURL");
+    private static int DangerZoneX1 = GetSettingInt("VTuberDangerZoneX1");
+    private static int DangerZoneX2 = GetSettingInt("VTuberDangerZoneX2");
+    private static int DangerZoneY1 = GetSettingInt("VTuberDangerZoneY1");
+    private static int DangerZoneY2 = GetSettingInt("VTuberDangerZoneY2");
+    private static int SafeZoneX1 = GetSettingInt("VTuberSafeZoneX1");
+    private static int SafeZoneX2 = GetSettingInt("VTuberSafeZoneX2");
+    private static int SafeZoneY1 = GetSettingInt("VTuberSafeZoneY1");
+    private static int SafeZoneY2 = GetSettingInt("VTuberSafeZoneY2");
 
     private static readonly string[] FirstSplit = { "\r\n", "\r", "\n" };
     private static readonly string[] SecondSplit = { ": " };
     private static readonly string[] ThirdSplit = { ", " };
 
     private static char oldWindDir = '0';
-    private static int oldWindValue = 0;
-    private static int oldHairCol = 0;
+    private static string oldWindValue = "";
+    private static string oldHairCol = "";
     private static bool oldDeadState = false;
     private static bool oldFeatherState = false;
     private static bool oldDashState = false;
@@ -69,7 +108,7 @@ public static class VNyanWebSocket {
     
     private static void CompareAndSend(ref string newValue, ref string oldValue, string message) {
         if (newValue != oldValue) {
-            message += newValue;
+            message += " " + newValue;
             wsClient.SendAsync(message, WebSocketMessageType.Text, CT);
             Log("String: " + message);
             if (wsConnected) { oldValue = newValue; }
@@ -104,9 +143,10 @@ public static class VNyanWebSocket {
         string[] result;
         int windX = 0;
         int windY = 0;
-        char windDir = '0';
-        int windValue = 0;
-        int hairCol = 0;
+        int windSpeed = 0;
+        // char windDir = '0';
+        string windValue = "";
+        string hairCol = "";
         bool deadState = false;
         bool featherState = false;
         bool dashState = false;
@@ -150,27 +190,10 @@ public static class VNyanWebSocket {
                 result = line.Split(SecondSplit, StringSplitOptions.None);
                 if (result[0] == "Wind") {
                     temp = result[1].Split(ThirdSplit, StringSplitOptions.None);
-                    windX = (int) Convert.ToDecimal(temp[0]) / 100;
-                    windY = (int) Convert.ToDecimal(temp[1]) / 100;
-                    if (windX == 0) {
-                        if (windY == 0) {
-                            windDir = '0';
-                        } else {
-                            if (windY < 0) {
-                                windDir = 'U';
-                            } else {
-                                windDir = 'D';
-                            }
-                            windValue = Math.Abs(windY);
-                        }
-                    } else {
-                        if (windX < 0) {
-                            windDir = 'L';
-                        } else {
-                            windDir = 'R';
-                        }
-                        windValue = Math.Abs(windX);
-                    }
+                    windX = (int) (0-(Convert.ToDecimal(temp[0]))); // Because VNyan wind is model relative, not world relative
+                    windY = (int) (0-(Convert.ToDecimal(temp[1])));
+                    windSpeed = (int) Math.Round(Math.Sqrt((windX * windX) + (windY * windY)));
+                    windValue = windX.ToString() + "," + windY.ToString() + "," + windSpeed.ToString();
                 } else if (result[0] == "Pos") {
                     temp = result[1].Split(ThirdSplit, StringSplitOptions.None);
                     globalXPos = (int) Convert.ToDecimal(temp[0]);
@@ -201,17 +224,17 @@ public static class VNyanWebSocket {
                         byte red = Convert.ToByte(RGBStr[0].Substring(2));
                         byte green = Convert.ToByte(RGBStr[1].Substring(2));
                         byte blue = Convert.ToByte(RGBStr[2].Substring(2));
-                        // hairCol = '#' + red.ToString("X2") + green.ToString("X2") + blue.ToString("X2");
+                        hairCol = "#" + red.ToString("X2") + green.ToString("X2") + blue.ToString("X2") +"FF";
                         // hairCol = "{ \"Red\": \"" + red.ToString() + "\", \"Green\": \"" + green.ToString() + "\"; \"blue\": \"" + blue.ToString() + "\" }";
-                        hairCol = +red * 65536 + green * 256 + blue;
+                        // hairCol = +red * 65536 + green * 256 + blue;
 
                     }
                 } else if (result[0].Contains("Timer")) {
                     roomName = result[0].Substring(0, result[0].IndexOf("Timer")-1);
-                } else if (result[0] == "Stamina") { // Status info is shown on the same line as stamina
+                } else if (result[0] == "Stamina") { // Status info is shown on the same line as stamina ( TODO: ST 20 = flashing)
                     string status = result[1].Substring(result[1].LastIndexOf(' ') + 1);
                     //MessageBox.Show("|"+status);
-                    switch (status) {
+                    switch (status) { //   TODO: stDreamSash to add   DashCD on own line = white hair
                         case "StStarFly":
                             featherState = true;
                             break;
@@ -241,11 +264,6 @@ public static class VNyanWebSocket {
             }
         }
 
-        if (!swimState && oldSwimState || (introJumpState && !oldIntroJumpState)) { // My swiming animation messes with wind, so force a reset to happen
-            oldWindDir = '-';
-            oldWindValue = 0;
-        }
-
         // Send state info to VNyan
 
         if (firstConnect) {
@@ -254,16 +272,7 @@ public static class VNyanWebSocket {
         }
 
         CompareAndSend(ref menuState, ref oldMenuState, "CelesteMenu");
-
-        if (oldWindDir != windDir) {
-            RawSend("CelesteWind" + windDir + ' ' + windValue.ToString());
-            if (wsConnected) {
-                oldWindDir = windDir;
-                oldWindValue = windValue;
-            }
-        } else {
-            CompareAndSend(ref windValue, ref oldWindValue, "CelesteWind" + windDir);
-        }
+        CompareAndSend(ref windValue, ref oldWindValue, "CelesteWind");
         CompareAndSend(ref deadState, ref oldDeadState, "CelesteDead");
         CompareAndSend(ref featherState, ref oldFeatherState, "CelesteFeather");
         CompareAndSend(ref dashState, ref oldDashState, "CelesteDash");
